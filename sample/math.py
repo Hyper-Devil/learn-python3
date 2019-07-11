5bbb78773e16cddc77b34bd0c45930ff77c7b114
Hyper-Devil <tywhd@qq.com>
2019-07-11 17:24:32 +0800

    表达式，input()存在问题

--- sample/math.py
+++ sample/math.py
@@ -1,6 +1,22 @@
-a = 1
-b = 2
+a = 6
+b = 4
 print(a + b)
 print(a - b)
 print(a * b)
 print(a / b)
+# python3特性
+print(a // b)
+# a整除b
+print(a**b)
+# a的b次方
+print(a % b)
+# a模b也就是a/b的余数
+
+print(a > b)
+print(a == b)
+print(a != b)
+# a不等于b
+
+a += b
+print(a)
+# a += b 等价于 a = a + b
