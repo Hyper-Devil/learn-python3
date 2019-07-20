984c8128c07b20dee66a256bd49ca67e2e5e493a
Hyper-Devil <tywhd@qq.com>
2019-07-20 16:47:52 +0800

    解决了导入路径问题

--- sample/12.import.py
+++ sample/12.import.py
@@ -1,10 +1,16 @@
 import os
 import old.mymodel
 # *需要__init__.py
+
+# from old import mymodel
+# print_hello()
+# *不推荐
+
 import sys
 sys.path.append('./')
 import test
-# !仍未解决的问题：如何加载任意目录下的模块
+# TODO 仍未解决的问题：如何加载任意目录下的模块
+# *解决
 
 path1 = os.path.abspath('.')
 # 当前目录
---
+++ sample/12.import_anyone.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('/Download')
+# 插件问题
+import test_download
+
+test_download.print_hello()
