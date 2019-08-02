1958fbb635e9b1287eed83b4d3489140a92b4819
Hyper-Devil <tywhd@qq.com>
2019-08-02 17:36:42 +0800

    av57661741

--- sample/16.urllib.py
+++ sample/16.urllib.py
@@ -1,5 +1,16 @@
-import urllib.request
+from urllib import request
 
-res = urllib.request.urlopen('http://www.yuqiaochuang.com/')
-ret = res.read().decode('utf-8')
-print(ret)
+# res = urllib.request.urlopen('http://www.yuqiaochuang.com/')
+# ret = res.read().decode('utf-8')
+# print(ret)
+
+url = 'http://www.yuqiaochuang.com/'
+
+headers = {
+    'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'
+}
+
+res = request.Request(url, headers=headers, method='POST')
+ret = request.urlopen(res)
+print(ret.read().decode('utf-8'))
---
+++ sample/22.fengmo.py
@@ -0,0 +1,46 @@
+import re
+import requests
+import baiduaip
+import time
+# from bs4 import BeautifulSoup
+# import lxml
+# import html
+
+start = time.time()
+temp_ocr = baiduaip.get_text_from_image()
+question = temp_ocr[0]
+ans1 = temp_ocr[1]
+ans2 = temp_ocr[2]
+ans3 = temp_ocr[3]
+# ans = []
+# ans.append(ans1)
+# ans.append(ans2)
+# ans.append(ans3)
+
+# url = "http://www.paopaoche.net/sj/120914.html"
+# res = requests.get(url)
+# res = res.content.decode('gbk')
+# body = html.unescape(res).replace("<br/>", "\n")
+print(question)
+
+
+with open('data.txt', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+# print(c)
+
+# result = re.match(r'\u98df\u53d1\u9b3c\u7684\u59d0\u59d0\u662f\u8c01', c)
+# if result:
+#     print(result.group())
+# else:
+#     print('No match!')
+
+# print(c.find('\u98df\u53d1\u9b3c\u7684\u59d0\u59d0\u662f\u8c01'))
+
+for line in lines:
+    # if line.find('\u98df\u53d1\u9b3c\u7684\u59d0\u59d0\u662f\u8c01'):
+    if line.find('食发鬼的姐姐是谁?'):
+        print(line)
+        break
+    
+end = time.time()
+print('程序用时：'+str(end-start)+'秒')
